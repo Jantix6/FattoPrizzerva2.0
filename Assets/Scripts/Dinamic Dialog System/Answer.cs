a09e36a2f77ee4f579c3d6169a212ca8979c0e26
TheMaker1993 <38749147+TheMaker1993@users.noreply.github.com>
2019-07-16 10:43:07 +0200

    [DANIEL] Commit Inicial

---
+++ Assets/Scripts/Dinamic Dialog System/Answer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dialogues
+{
+    [CreateAssetMenu(fileName = "Answer", menuName = "Answers/New Answer")]
+    public class Answer : ScriptableObject
+    {
+        [SerializeField] [TextArea] private string answerBody;
+        [SerializeField] private QuestionAnswerStructure targetStructure;
+
+        public string GetAnswerBody()
+        {
+            return answerBody;
+        }
+        public QuestionAnswerStructure GetTargetStructure()
+        {
+            return targetStructure;
+        }
+    }
+
+}
+
