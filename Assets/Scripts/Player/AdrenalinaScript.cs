3fdd725ec4da745c365e31d2d37995f3900c303e
SerTor2 <sertor27@gmail.com>
2019-07-25 11:34:32 +0200

    Scriptable object y Adrenalina

---
+++ Assets/Scripts/Player/AdrenalinaScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdrenalinaScript : MonoBehaviour, IAdrenalinable
+{
+
+    [SerializeField] private float currentAdrenalina;
+    [SerializeField] private float maxAdrenalina = 100;
+
+    public float decreseAdrenalinaPerSecond = 5f;
+
+    public float Adrenalina { get => currentAdrenalina; set => currentAdrenalina = value; }
+    public float MaxAdrenalina { get => maxAdrenalina; set => maxAdrenalina = value; }
+
+    public void ReduceAdrenalina()
+    {
+        currentAdrenalina -= decreseAdrenalinaPerSecond * Time.deltaTime;
+    }
+}
