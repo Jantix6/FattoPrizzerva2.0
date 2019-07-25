abfb065b824aedd4f30f924b42fd2bb90f048336
Albert Pujante <apujante@edu.tecnocampus.cat>
2019-07-25 10:10:17 +0200

    UI manager

---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+#if UNITY_STANDALONE_WIN
+        Application.Quit();
+#endif
+    }
+}
