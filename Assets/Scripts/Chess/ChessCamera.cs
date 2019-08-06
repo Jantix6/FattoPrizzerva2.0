f1832cda76c525aa07b37636b37690305189d305
Albert Pujante <apujante@edu.tecnocampus.cat>
2019-08-06 11:14:14 +0200

    Update form master

---
+++ Assets/Scripts/Chess/ChessCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessCamera : MonoBehaviour
+{
+    public enum State { FREE, LOCKED, FOLLOWING };
+    public State currentState = State.LOCKED;
+    private MovementPreview pieceInMovement;
+
+    public void Awake()
+    {
+        pieceInMovement = GetComponent<MovementPreview>();
+        ChangeState(State.LOCKED);
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void ChangeState(State _newState)
+    {
+        switch (_newState)
+        {
+            case State.FREE:
+                break;
+            case State.LOCKED:
+                break;
+            case State.FOLLOWING:
+                break;
+
+        }
+        currentState = _newState;
+    }
+}
