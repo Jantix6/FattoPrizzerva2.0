2986411f00665aac60e8923e202d92505eeaecc1
Àlex <alexmrtc@gmail.com>
2019-10-02 20:47:07 +0200

    UI Horse example & script

--- Assets/Tests/Horses/Horses.cs
+++ Assets/Tests/Horses/Horses.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Horses : MonoBehaviour
 {
+    public int posX;
+    public int posY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,53 @@ public class Horses : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void DownLeft()
+    {
+        posX = posX - 1;
+        posY = posY - 2;
+    }
+
+    public void DownRight()
+    {
+        posX = posX + 1;
+        posY = posY - 2;
+    }
+
+    public void UpLeft()
+    {
+        posX = posX - 1;
+        posY = posY + 2;
+    }
+
+    public void UpRight()
+    {
+        posX = posX + 1;
+        posY = posY + 2;
+    }
+
+    public void LeftUp()
+    {
+        posX = posX - 2;
+        posY = posY + 1;
+    }
+
+    public void LeftDown()
+    {
+        posX = posX - 2;
+        posY = posY - 1;
+    }
+
+    public void RightUp()
+    {
+        posX = posX + 2;
+        posY = posY + 1;
+    }
+
+    public void RightDown()
+    {
+        posX = posX + 2;
+        posY = posY - 1;
     }
 }
